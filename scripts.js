c511e822d004e812f3f819b7a869a43a53921a04
theskillwithin <austinppeterson@gmail.com>
2014-10-21 21:05:51 -0700

    Hamburger Menu FTW!

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 // @codekit-prepend "jquery-2.1.1.min.js"
 
-function checkWidth() {
+
+// verticle responce
+function checkHeight() {
 	var windowHeight = $(window).height();
 		if (windowHeight < 600) {
 			$( ".nameplate > a" ).slideUp();
@@ -8,8 +10,43 @@ function checkWidth() {
 			$( ".nameplate > a" ).slideDown();
 		}
 }
-checkWidth();
-$(window).resize(checkWidth);
+checkHeight();
+$(window).resize(checkHeight);
 
-var images = ['green.jpg', 'lake.jpg', 'ocean.jpg'];
+// random background
+var images = ['green.jpg', 'ocean.jpg'];
 $('html').css({'background': 'url(assets/bg/' + images[Math.floor(Math.random() * images.length)] + ')' + 'no-repeat center center fixed ', 'background-size': 'cover'});
+
+// hamburger
+$('#menu-btn').addClass('.open');
+
+
+$('#menu-btn').click(function(){
+
+	
+	if ( $(this).hasClass('.open') ) {
+		
+		$('#site-menu').slideUp();
+		$('.window').css("margin-top", "32px");
+		$('#menu-btn').removeClass('.open');
+		
+	} else {
+		$('#site-menu').slideDown();
+		$('.window').css("margin-top", "170px");
+		$('#menu-btn').addClass('.open');
+}
+});
+
+	// hamburger > checkwidth
+	function checkWidth() {
+		var windowWidth = $(window).width();
+			if (windowWidth > 699) {
+				$('.window').css("margin-top", "5px");
+				$('#site-menu').slideDown();
+				$('#menu-btn').addClass('.open');
+			} else {
+				$('.window').css("margin-top", "170px");
+			}
+	}
+	checkWidth();
+	$(window).resize(checkWidth);
